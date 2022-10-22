11f6def92b78774a644bd187d9c235c647248cfa
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-10-22 10:47:12 +0000

    feat: :sparkles: Create web page base

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="increment">Click Me!</button>
+  <div>Clicked: {{ count }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+//#region Counter
+const count = ref(0);
+const increment = () => (count.value += 1);
+//#endregion
+</script>
