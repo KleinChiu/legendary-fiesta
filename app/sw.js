d06843eb1236e8f1596632730be3532a73206c33
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-10-24 02:05:57 +0000

    fix(pwa): :zap: Add cache first strategy
    for offline capability

--- app/sw.js
+++ app/sw.js
@@ -1,7 +1,11 @@
+/// <reference lib="WebWorker" />
+
 /**
  * @type {ServiceWorkerGlobalScope}
  */
 const sw = self;
+
+const cacheName = "demo-ProgramData";
 
 /**
  *
@@ -9,17 +13,40 @@ const sw = self;
  */
 const installer = (ev) => {
   caches
-    .open("demo-ProgramData")
+    .open(cacheName)
     .then((cache) =>
       cache.addAll(self.__WB_MANIFEST.map((asset) => asset.url))
     );
 };
 
 /**
- *
- * @param {FetchEvent} ev
+ * Cache clone of responses and return the original.
+ * @param {Request} req
+ * @returns {Promise<Response>}
  */
-const apiFetch = (ev) => {};
+const cacheResponse = (req) => (response) =>
+  caches
+    .open(cacheName)
+    .then((cache) => cache.put(req, response.clone()))
+    .then(() => response);
 
-self.addEventListener("install", installer);
-self.addEventListener("fetch", apiFetch);
+/**
+ * Fetch and Cache requests.
+ * @param {Request} req
+ * @returns {Promise<Response>}
+ */
+const fetchAndCache = (req) => fetch(req).then(cacheResponse(req));
+
+/**
+ * Cache first strategy.
+ * @param {Request} req
+ * @returns {Promise<Response>}
+ */
+const cacheFirst = (req) =>
+  caches
+    .match(req)
+    .then((res) => (res == undefined ? Promise.reject(req) : res))
+    .catch(fetchAndCache);
+
+sw.addEventListener("install", installer);
+sw.addEventListener("fetch", (ev) => ev.respondWith(cacheFirst(ev.request)));
