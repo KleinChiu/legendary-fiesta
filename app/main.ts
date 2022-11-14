bc3f39ce5127ece8b4a08f055c93ccd8d26be3be
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-11-14 07:55:18 +0000

    refactor: :recycle: Extract i18n to plugin

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from "vue";
-import { createI18n } from "vue-i18n";
+
+import i18n from "../plugin/i18n";
 
 import App from "./App.vue";
-
-import en from "../public/i18n/en/message.json";
-import ja from "../public/i18n/ja/message.json";
-
-const i18n = createI18n({
-  locale: "en",
-  fallbackLocale: "en",
-  messages: { en, ja },
-});
 
 createApp(App).use(i18n).mount("#app");
