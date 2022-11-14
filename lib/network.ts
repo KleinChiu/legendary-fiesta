5b8a860f2bd9f1ee094b97757a92c4baa92cfbed
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-11-14 08:02:29 +0000

    refactor: :recycle: Change API server URL
    So that it can include locales alongside data

--- lib/network.ts
+++ lib/network.ts
@@ -1,15 +1,18 @@
 import { Persona, User } from "../data";
+import { MessageSchema, SupportLocales } from "../plugin/i18n";
 
 export const fetchPersona = () =>
-  fetch(`${process.env.API_SERVER}/personas.json`)
+  fetch(`${process.env.API_SERVER}/data/personas.json`)
     .then((data) => data.json())
     .then((data) => data.personas as Persona[])
     .catch(() => null);
 
 export const login = () =>
-  fetch(`${process.env.API_SERVER}/user.json`)
+  fetch(`${process.env.API_SERVER}/data/user.json`)
     .then<User>((data) => data.json())
     .catch(() => null);
 
 export const loadLocale = (locale: SupportLocales) =>
-  fetch(`locales/${locale}.json`).then<MessageSchema>((data) => data.json());
+  fetch(`${process.env.API_SERVER}/locales/${locale}.json`).then<MessageSchema>(
+    (data) => data.json()
+  );
